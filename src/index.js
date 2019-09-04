bba3b57d40e389ff47a37484110b90c7811190c0
Mike Stemle <mike.stemle@washpost.com>
2019-09-03 22:01:27 -0400

    Cleaning up the demo a little more.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ import "./styles.css";
 export const App = () => {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <ZeusAd slotId="zeus_mob_bigbox_1" style={{ fontSize: "+1" }} />
+      <h1>Zeus Ad Demo</h1>
+      <p>
+        Below, you should see a single ad. If you don't see one, please verify
+        you don't have any privacy or ad blockers enabled.
+      </p>
+      <ZeusAd slotId="zeus_mob_bigbox_1" />
     </div>
   );
 };
