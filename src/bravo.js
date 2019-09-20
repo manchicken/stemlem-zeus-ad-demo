02b86e8570ead747734ef1965e3a1c01411a41f1
Mike Stemle <mike.stemle@washpost.com>
2019-09-20 14:47:02 -0400

    Playing with an SPA bug in ZeusAd.

---
+++ src/bravo.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+export const Bravo = () => (
+  <Fragment>
+    <h1>Bravo</h1>
+    <Link to="/">To Alpha</Link>
+  </Fragment>
+);
